43ef63c8546402c8b30262b087f48e374d544fd2
minhtuan1611 <tuan.mn51@gmail.com>
2023-11-01 15:11:52 +0200

    Fix: Content

--- src/app/page.js
+++ src/app/page.js
@@ -96,14 +96,14 @@ export default function Home() {
               <h1>Skills</h1>
               <ul className="skills-list">
                 <li>Node.js</li>
+                <li>ReactJS</li>
+                <li>Python</li>
                 <li>TypeScript</li>
                 <li>ReactJS</li>
-                <li>Docker</li>
                 <li>HTML</li>
                 <li>CSS</li>
                 <li>MongoDB</li>
                 <li>Wordpress</li>
-                <li>Python</li>
               </ul>
               <p className="skills-description">
                 I possess a diverse skill set encompassing various technologies
